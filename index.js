7d67c99aa0e9885257a333acd556efe053a901ac
Erick Wendel <erick.workspace@gmail.com>
2017-11-04 23:12:51 +0000

    test

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 class Products {
-    create() {
-        const a = db.products.create({
-            user: { name: 'erick' }, product: {
-                price: 23.0, description: 'Mortadela', barcode: { code: '123123123' },
-                date: '2013/01/01'
-            },
-        })
-    }
-    list() {
-        db.
-        products.find({ 'user.name': 'erick', 'product.price': { $gt: 10 }, })
-    }
+  const b = create() { db.products.create({ user: { name: 'erick' }, product: { price: 23.0, description: 'Mortadela', barcode: { code: '123123123' }, date: '2013/01/01' }, }) }
+  list() {
+    db.products.find({ 'user.name': 'erick', 'product.price': { $gt: 10 }, })
+  }
 }
